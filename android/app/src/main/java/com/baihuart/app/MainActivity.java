0f7785df8a7420e3d4846b0e78397e1c227c5bc1
German Rodriguez <g3rm4n7115@gmail.com>
2026-08-27 12:29:03 -0500

    agrega barra de navegacion inferior estilo app

---
+++ android/app/src/main/java/com/baihuart/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.baihuart.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.baihuart.app',
+  appName: 'Baihuart',
+  webDir: 'dist/baihuangular'
+};
+
+export default config;
